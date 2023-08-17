9500369e48a3462986790e601e0403e3cd5f4b1d
EvanDHeller <134806751+EvanDHeller@users.noreply.github.com>
2023-08-17 16:18:26 -0500

    Update inspiredquotes.js
    bolds the authors name, and any future authors name allowing you to add any additional quotes on one line maintaining the same format

--- projects/inspiredquotes/inspiredquotes.js
+++ projects/inspiredquotes/inspiredquotes.js
@@ -1,10 +1,23 @@
 const btn = document.getElementById('btn');
 const output = document.getElementById('output');
 const quote = [
-  `"If you push something away, it's still got you. You're busy not doing it" -Ram Dass`,
+  `"If you push something away, it's still got you. You're busy not doing it" Ram Dass`,
   `"When the flower blooms, the bees come uninvited." -Ramakrishna`,
 `"As long as you have some desire about how you think it ought to be, you can't hear how it is." -Ram Dass` ,
 ];
+
+// Function to format the author's name with bold
+function formatAuthor(entry) {
+  let parts = entry.split(' -'); // Split at the em-dash
+  if (parts.length === 2) {
+    return parts[0] + ' -<strong>' + parts[1] + '</strong>'; // Bold the author's name
+  } else {
+    return entry; // Return unchanged if formatting fails
+  }
+}
+
+// Iterate through the entries and format the author's names
+quote = quote.map(formatAuthor);
 
 btn.addEventListener('click', () => {
   let randomQuote = quote[Math.floor(Math.random() * quote.length)];
